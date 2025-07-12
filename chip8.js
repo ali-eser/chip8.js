cfee46ba09ca6636a10a53787659f6534d8145f7
ali-eser <115903331+ali-eser@users.noreply.github.com>
2025-07-13 02:01:34 +0300

    Implement key handling, fix FX55-FX65, fix display

--- chip8.js
+++ chip8.js
@@ -9,10 +9,14 @@ const writeToMemory = (data, address) => {
   }
 };
 
+const cleanMemory = () => {
+  for (let i = 0x200; i < 4096; i++) {
+    mem[i] = 0x0;
+  }
+}
+
 let I;
-
 let PC = 0x200;
-
 let vReg = new Uint8Array(16);
 
 // define 32 bytes of stack
@@ -30,6 +34,25 @@ setInterval(() => {
     soundTimer--;
   }
 }, 1000/60);
+
+let keyLog = {
+  "KeyX": false, 
+  "Digit1": false, 
+  "Digit2": false, 
+  "Digit3": false,
+  "KeyQ": false, 
+  "KeyW": false, 
+  "KeyE": false, 
+  "KeyA": false,
+  "KeyS": false,
+  "KeyD": false,
+  "KeyZ": false,
+  "KeyC": false, 
+  "Digit4": false,
+  "KeyR": false,
+  "KeyF": false,
+  "KeyV": false
+};
 
 // create a 64x32 display and append to the viewport element
 const viewport = document.querySelector("#viewport");
@@ -76,8 +99,10 @@ document.querySelector("#run-file").addEventListener("click", () => {
   }
 
   reader.onload = (e) => {
+    cleanMemory();
     writeToMemory(new Uint8Array(e.target.result), 0x200);
     setInterval(() => {
+      watchKeyEvents();
       fetch();
     }, 1000/660);
   };
@@ -91,7 +116,6 @@ document.querySelector("#run-file").addEventListener("click", () => {
 
 const fetch = () => {
   const opcode = (mem[PC] << 8) | mem[PC + 1];
-  //console.log("OPCODE: ", opcode.toString(16).toUpperCase());
   PC = PC + 2;
   decode(opcode);
 }
@@ -105,7 +129,6 @@ const decode = (opcode) => {
           px.forEach((p) => {
             p.style.backgroundColor = "rgb(0, 0, 0)";
           });
-          //console.log("cleared screen");
           break;
         case 0x00EE:
           PC = stack.pop();
@@ -114,13 +137,10 @@ const decode = (opcode) => {
       break;
     case 0x0001:
       PC = opcode & 0x0FFF;
-      //console.log("PC set to: ", PC);
       break;
     case 0x0002:
       stack.push(PC);
-      //console.log("pushed to stack: ", PC);
       PC = opcode & 0x0FFF;
-      //console.log("PC set to: ", PC);
       break;
     case 0x0003:
       if (vReg[(opcode & 0x0F00) >> 8] === (opcode & 0x00FF)) {
@@ -139,11 +159,9 @@ const decode = (opcode) => {
       break;
     case 0x0006:
       vReg[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
-      //console.log(`${vReg[(opcode & 0x0F00) >> 8]} set to ${opcode & 0x00FF}`);
       break;
     case 0x0007:
-      console.log(opcode.toString(16).toUpperCase());
-      vReg[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+      vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x0F00) >> 8] + opcode & 0x00FF;
       break;
     case 0x0008:
       switch (opcode & 0x000F) {
@@ -160,35 +178,36 @@ const decode = (opcode) => {
           vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x0F00) >> 8] ^= vReg[(opcode & 0x00F0) >> 4];
           break;
         case 0x0004:
-          vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x0F00) >> 8] + vReg[(opcode & 0x00F0) >> 4];
-          if (vReg[(opcode & 0x0F00) >> 8] > 255) {
+          if (vReg[(opcode & 0x0F00) >> 8] + vReg[(opcode & 0x00F0) >> 4] > 255) {
             vReg[0xF] = 1;
           } else {
             vReg[0xF] = 0;
           }
+          vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x0F00) >> 8] + vReg[(opcode & 0x00F0) >> 4];
           break;
         case 0x0005:
-          if (vReg[(opcode & 0x0F00) >> 8] > vReg[(opcode & 0x00F0) >> 4]) {
+          const x = (opcode & 0x0F00) >> 8;
+          const y = (opcode & 0x00F0) >> 4;
+          if (vReg[x] > vReg[y]) {
             vReg[0xF] = 1;
-          } else {
+          } else if (vReg[x] < vReg[y]) {
             vReg[0xF] = 0;
           }
-          vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x0F00) >> 8] - vReg[(opcode & 0x00F0) >> 4];
+          vReg[x] = vReg[x] - vReg[y];
           break;
         case 0x0006:
           vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x00F0) >> 4];
           vReg[(opcode & 0x0F00) >> 8] >>= 1;
           break;
         case 0x0007:
-          if (vReg[(opcode & 0x00F0) >> 4] > vReg[(opcode & 0x0F00) >> 8]) {
-            vReg[0xF] = 1;
-          } else {
+          vReg[0xF] = 1;
+          vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x00F0) >> 4] - vReg[(opcode & 0x0F00) >> 8];
+          if (vReg[(opcode & 0x00F0) >> 4] < vReg[(opcode & 0x0F00) >> 8]) {
             vReg[0xF] = 0;
           }
-          vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x00F0) >> 4] - vReg[(opcode & 0x0F00) >> 8];
           break;
         case 0x000E:
-          //vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x00F0) >> 4];
+          vReg[(opcode & 0x0F00) >> 8] = vReg[(opcode & 0x00F0) >> 4];
           vReg[(opcode & 0x0F00) >> 8] <<= 1;
           break;
       }
@@ -198,8 +217,7 @@ const decode = (opcode) => {
       }
       break;
     case 0x000A:
-      I = opcode & 0x0FFF;
-      //console.log("set I to ", I);
+      I = (opcode & 0x0FFF);
       break;
     case 0x000B:
       PC = (opcode & 0x0FFF) + vReg[0x0];
@@ -210,34 +228,102 @@ const decode = (opcode) => {
       vReg[(opcode & 0x0F00) >> 8] = randint;
       break;
     case 0x000D:
-      let x = vReg[(opcode & 0x0F00) >> 8] & 63;
-      let y = vReg[(opcode & 0x00F0) >> 4] & 31;
+      let y = vReg[(opcode & 0x00F0) >> 4] % 32;
       vReg[0xF] = 0;
       for (let i = 0; i < (opcode & 0x000F); i++) {
-        if (i > 0) {
-          x -= 8; 
-        }
-        if (y > 31) {
-          break;
-        }
+        let x = vReg[(opcode & 0x0F00) >> 8] % 64;
         let sprite = mem[I + i];
-        for (let j = 0; j < 8; j++) {    
-          if (x > 63) {
-            break;
-          }
+        for (let j = 0; j < 8; j++) {
           let currPix = (sprite & (1 << 7 - j)) != 0;
           const scrPixel = document.querySelector(`#x${x}-y${y}`);
-          if (scrPixel.style.backgroundColor === "rgb(255, 255, 255)" && currPix) {
+          if (scrPixel.style.backgroundColor === "rgb(0, 255, 0)" && currPix) {
             scrPixel.style.backgroundColor = "rgb(0, 0, 0)";
             vReg[0xF] = 1;
           } else if (scrPixel.style.backgroundColor === "rgb(0, 0, 0)" && currPix) {
-            scrPixel.style.backgroundColor = "rgb(255, 255, 255)";
+            scrPixel.style.backgroundColor = "rgb(0, 255, 0)";
             vReg[0xF] = 0;
           }
           x++;
+          if (x > 63) {
+            break;
+          }
         }
         y++;
+        if (y > 31) {
+          break;
+        }
       }
       break;
+    case 0x000E:
+      switch (opcode & 0x00FF) {
+        case 0x009E:
+          if (Object.values(keyLog)[(opcode & 0x0F00) >> 8]) {
+            PC += 2;
+          }
+          break;
+        case 0x00A1:
+          if (!Object.values(keyLog)[(opcode & 0x0F00) >> 8]) {
+            PC += 2;
+          }
+          break;
+      }
+    case 0x000F:
+      switch (opcode & 0x00FF) {
+        case 0x0007:
+          vReg[(opcode & 0x0F00) >> 8] = delayTimer;
+          break;
+        case 0x0015:
+          delayTimer = vReg[(opcode & 0x0F00) >> 8];
+          break;
+        case 0x0018:
+          soundTimer = vReg[(opcode & 0x0F00) >> 8];
+          break;
+        case 0x001E:
+          I += vReg[(opcode & 0x0F00) >> 8];
+          break;
+        case 0x000A:
+          let pressedKey = Object.values(keyLog).indexOf(true);
+          if (pressedKey !== -1) {
+            vReg[(opcode & 0x0F00) >> 8] = pressedKey;
+          } else {
+            PC -= 2;
+          }
+          break;
+        case 0x0029:
+          I = 0x50 + (((opcode & 0x0F00) >> 8) * 5);
+          break;
+        case 0x0033:
+          let i = 100;
+          let count = 0
+          while (i >= 1) {
+            mem[I + count] = Math.floor(((vReg[(opcode & 0x0F00) >> 8]) / i) % 10);
+            i /= 10;
+            count++;
+          }
+          break;
+        case 0x0055:
+          for (let i = 0; i <= ((opcode & 0x0F00) >> 8); i++) {
+            mem[I + i] = vReg[i];
+          }
+          break;
+        case 0x0065:
+          for (let i = 0; i <= ((opcode & 0x0F00) >> 8); i++) {
+            vReg[i] = mem[I + i];
+          }
+          break;
+      }
+  }
+};
+
+const watchKeyEvents = () => {
+  onkeydown = (e) => {
+    if (e.code in keyLog) {
+      keyLog[e.code] = true;
+    }
+  }
+  onkeyup = (e) => {
+    if (e.code in keyLog) {
+      keyLog[e.code] = false;
+    }
   }
 };
